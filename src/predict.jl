6d7f63574b412a3857a0678979a77d9b6953bb22
Clint Leach <clint.leach@gmail.com>
2021-08-25 11:10:30 -0400

    Computing running mean of log predictions

--- src/predict.jl
+++ src/predict.jl
@@ -1,29 +1,31 @@
-function predict(chain, iters, m)
+function predict(chain, m)
 
     @unpack X_all, X, Σuo_r, Ω_r, Σuu_r, Σuo_0, Ω_0, Σuu_0, λ_all, T = m
 
-    # nmcmc = length(chain["sigma"])
-    nmcmc = length(iters)
+    nmcmc = length(chain["sigma"])
     npred = size(X_all, 1)
 
     # Building storage arrays
-    r_pred = fill(0.0, npred, nmcmc)
-    u_pred = fill(0.0, T, npred, nmcmc)
+    log_r_pred = fill(0.0, npred)
+    log_u_pred = fill(0.0, T, npred)
+    log_flux_pred = fill(0.0, T, npred)
 
     Σ_pred_r = PDMat(Symmetric(Σuu_r - Σuo_r * Ω_r * Σuo_r'))
     Σ_pred_0 = PDMat(Symmetric(Σuu_0 - Σuo_0 * Ω_0 * Σuo_0'))
 
     for i in 1:nmcmc
 
-        @show i
+        if i % 500 == 0
+			@show i
+		end
 
-        β_r = chain["beta_r"][:, iters[i]]
-        β_0 = chain["beta_0"][:, iters[i]]
-        η_r = chain["eta_r"][:, iters[i]]
-        η_0 = chain["eta_0"][:, iters[i]]
-        a = chain["a"][iters[i]]
-        κ = chain["kappa"][iters[i]]
-        K = chain["K"][iters[i]]
+        β_r = chain["beta_r"][:, i]
+        β_0 = chain["beta_0"][:, i]
+        η_r = chain["eta_r"][:, i]
+        η_0 = chain["eta_0"][:, i]
+        a = chain["a"][i]
+        κ = chain["kappa"][i]
+        K = chain["K"][i]
 
         # Compute μ_pred for r and K
         μ_pred_r = X_all * β_r + Σuo_r * Ω_r * (η_r - X * β_r)
@@ -40,14 +42,23 @@ function predict(chain, iters, m)
         p =  DEparams(r, a, κ, K, λ_all)
         u = process_all(p, u0, m)
 
-        # Return r and K vectors and U matrix
-        r_pred[:, i] = r
-        u_pred[:, :, i] = u
+        flux = similar(u)
+        for j in 1:npred
+            for t in 1:T
+                u[t, j] = u[t, j] > 0 ? u[t, j] : 1e-10
+                flux[t, j] = a * u[t, j] * λ_all[j](t)  / (u[t, j] + κ)
+            end
+        end
 
+        # Compute running means of r, u, and flux
+        log_r_pred = log_r_pred + 1.0 / nmcmc * η_r_pred
+        log_u_pred = log_u_pred + 1.0 / nmcmc * log.(u)
+        log_flux_pred = log_flux_pred + 1.0 / nmcmc * log.(flux)
     end
 
-    preds = Dict("r" => r_pred,
-                 "u" => u_pred)
+    preds = Dict("log_r" => log_r_pred,
+                 "log_u" => log_u_pred,
+                 "log_flux" => log_flux_pred)
 
     return preds
 end
