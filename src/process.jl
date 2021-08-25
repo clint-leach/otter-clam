6031a02d4800c5f271470e802820565ee9bb69bb
Clint Leach <clint.leach@gmail.com>
2021-08-25 11:16:02 -0400

    Dropping single site process code
    As it is no longer used

--- src/process.jl
+++ src/process.jl
@@ -1,24 +1,3 @@
-# Code defining and solving the process model
-
-# Rosenzweig-MacArthur model of single site prey dynamics
-function prey!(du, u, p, t)
-	@unpack log_r, log_K, a, κ, λ = p
-
-	du[1] = exp(log_r[1]) * u[1] * (1.0 - u[1] / exp(log_K[1])) - a * u[1] * λ[1](t) / (u[1] + κ)
-end
-
-function process_site(p, u0, m)
-
-	@unpack tspan, λ, N = m
-
-	prob = ODEProblem(prey!, [u0], tspan, p)
-
-	sol = solve(prob, Tsit5(), abstol = 1e-6, reltol = 1e-4, saveat=1.0)
-
-	return sol[1, :]
-
-end
-
 # Rosenzweig-MacArthur model of multi-site prey dynamics
 function prey_all!(du, u, p, t)
 	@unpack r, a, κ, K, λ = p
@@ -26,7 +5,6 @@ function prey_all!(du, u, p, t)
 	for i in 1:length(u)
 		du[i] = r[i] * u[i] * (1.0 - u[i] / K) - a * λ[i](t) * u[i] / (u[i] + κ)
 	end
-
 end
 
 # Solving ODE given parameters and λ
